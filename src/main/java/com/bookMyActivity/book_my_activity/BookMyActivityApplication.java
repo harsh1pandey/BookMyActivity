41bd0a607cb288cbe4df2f64bd76f5f9b7c1b05d
Harsh Pandey <harsh1_pandey@yahoo.com>
2025-07-23 09:15:01 -0400

    initialize repo

---
+++ src/main/java/com/bookMyActivity/book_my_activity/BookMyActivityApplication.java
@@ -0,0 +1,13 @@
+package com.bookMyActivity.book_my_activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyActivityApplication.class, args);
+	}
+
+}
